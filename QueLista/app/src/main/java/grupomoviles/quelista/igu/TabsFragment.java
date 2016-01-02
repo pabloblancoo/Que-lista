90d36469910eeba60895f5603d6fbae9a0a23e28
Carlos Alperi Meana <alperi89@gmail.com>
2016-01-02 22:09:20 +0100

    NavigationView funcionando ya al 100% correctamente. Era un bug de Design Support Library 22.2.1

--- QueLista/app/src/main/java/grupomoviles/quelista/igu/TabsFragment.java
+++ QueLista/app/src/main/java/grupomoviles/quelista/igu/TabsFragment.java
@@ -11,69 +11,62 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import grupomoviles.quelista.R;
 
-
-/**
- * Created by Nauce on 30/12/15.
- */
 public class TabsFragment extends Fragment {
 
     private static TabLayout tabLayout;
     private static ViewPager viewPager;
     private int int_items = 3 ;
     private static FragmentPagerAdapter myAdapter;
+    private int tab = 0;
+    private View v = null;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-
         /**
          * Inflamos fragment_tab y adjuntamos Views.
          */
-        View v =  inflater.inflate(R.layout.fragment_tabs,null);
+        v =  inflater.inflate(R.layout.fragment_tabs,null);
         tabLayout = (TabLayout) v.findViewById(R.id.tab_layout);
         viewPager = (ViewPager) v.findViewById(R.id.viewpager);
-
 
 
         /**
          * Establecemos un Adapter para el ViewPager
          */
         myAdapter = new MyAdapter(this.getChildFragmentManager());
+
         viewPager.setAdapter(myAdapter);
 
+        tabLayout.setOnTabSelectedListener(null);
+        viewPager.setCurrentItem(tab);
         tabLayout.setupWithViewPager(viewPager);
 
+
+        // Esto es necesario debido a un bug presente en la Design Support Library 22.2.1
         tabLayout.post(new Runnable() {
             @Override
             public void run() {
                 tabLayout.setupWithViewPager(viewPager);
+                viewPager.setCurrentItem(tab);
             }
         });
-
-        /**
-         * Configuramos el ActionBarDrawerToggle
-         */
-        Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbar);
-        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(getActivity(), ((MainActivity)getActivity()).mDrawerLayout,
-                toolbar, R.string.open_drawer, R.string.close_drawer);
-        ((MainActivity)getActivity()).mDrawerLayout.setDrawerListener(mDrawerToggle);
-        mDrawerToggle.syncState();
 
         return v;
 
     }
 
     public void setTab(int tab) {
-       viewPager.setCurrentItem(tab);
-
-
+        this.tab = tab;
     }
 
     class MyAdapter extends FragmentPagerAdapter {
